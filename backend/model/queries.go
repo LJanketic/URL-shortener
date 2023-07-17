e2239036de28310efbb7a552cc3eeb1c7d04bf2c
Luka <luka.janketic@gmail.com>
2023-07-17 14:26:27 +0200

    add more requests (not tested)

--- backend/model/queries.go
+++ backend/model/queries.go
@@ -4,7 +4,6 @@ func getAllMinfyers() ([]Minifyr, error) {
 	var minifyers []Minifyr
 
 	tx := db.Find(&minifyers)
-
 	if tx.Error != nil {
 		return []Minifyr{}, tx.Error
 	}
@@ -16,10 +15,27 @@ func getMinifyr(id uint64) (Minifyr, error){
 	var minifyr Minifyr
 
 	tx := db.Where("id = ?", id).First(&minifyr)
-
 	if tx.Error != nil {
 		return Minifyr{}, tx.Error
 	}
 
 	return minifyr, nil
+}
+
+func createMinifyr(minifyr Minifyr) error {
+	tx := db.Create(&minifyr)
+
+	return tx.Error
+}
+
+func updateMinifyr(minifyr Minifyr) error {
+	tx := db.Save(&minifyr)
+
+	return tx.Error
+}
+
+func deleteMinifyr(id uint64) error {
+	tx := db.Unscoped().Delete(&Minifyr{}, id)
+
+	return tx.Error
 }
