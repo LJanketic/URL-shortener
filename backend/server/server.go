4c1fdf2d505982da8dd62f98de43aa8d5416b204
Luka <luka.janketic@gmail.com>
2023-07-17 15:15:07 +0200

    Add server file

--- backend/server/server.go
+++ backend/server/server.go
@@ -1,9 +1,23 @@
 package server
 
 import (
+	"Minifyr/model"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+func getAllRedirects(ctx *fiber.Ctx) error {
+
+	minifyrs, err := model.GetAllMinfyrs()
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "Error getting all minifyr links " + err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(minifyrs)
+}
 
 func setupServerListener() {
 
