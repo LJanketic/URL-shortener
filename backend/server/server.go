2b753ba43056169aff6d79f111e0f81a06f9c7d3
Luka <luka.janketic@gmail.com>
2023-07-18 11:23:50 +0200

    Add get single minifyr from db

--- backend/server/server.go
+++ backend/server/server.go
@@ -2,21 +2,41 @@ package server
 
 import (
 	"Minifyr/model"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func getAllMinifyrs(ctx *fiber.Ctx) error {
+func getAllMinifyrs(c *fiber.Ctx) error {
 
 	minifyrs, err := model.GetAllMinfyrs()
 	if err != nil {
-		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
 			"message": "Error getting all minifyr links " + err.Error(),
 		})
 	}
 
-	return ctx.Status(fiber.StatusOK).JSON(minifyrs)
+	return c.Status(fiber.StatusOK).JSON(minifyrs)
+}
+
+func getMinifyr(c *fiber.Ctx) error {
+
+	id, err := strconv.ParseUint(c.Params("id"), 10, 64)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "Error could not parse id: " + err.Error(),
+		})
+	}
+
+	minifyr, err := model.GetMinifyr(id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map {
+			"message": "Error could not retrieve minfyr from db: " + err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(minifyr)
 }
 
 func SetupServerListener() {
@@ -29,5 +49,6 @@ func SetupServerListener() {
 	}))
 
 	router.Get("/getMinifyr", getAllMinifyrs)
+	router.Get("/getMinifyr/:id", getMinifyr)
 	router.Listen(":3000")
 }
