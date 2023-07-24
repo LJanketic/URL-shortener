5518c1ce876b72a8bc53a35865833bdf539300fe
Luka <luka.janketic@gmail.com>
2023-07-24 19:45:52 +0200

    add string utils

---
+++ backend/utils/strings.go
@@ -0,0 +1,15 @@
+package utils
+
+// server request error related strings
+var ERR_PARSE_JSON = "Error parsing JSON"
+var ERR_GET_ALL = "Get All request from database failed"
+var ERR_GET_ONE = "Get one request from database failed"
+var ERR_CREATE_ONE = "Create one in database failed"
+var ERR_UPDATE_ONE = "Update one in database failed"
+var ERR_DELETE_ONE = "Delete one in database failed"
+
+// 'clicked' property related messages
+var CLKD_ERR = "Incrementing 'Clicked' property failed"
+
+// confirmation server messages
+var SCS_DELETE_ONE = "Successfully deleted instance"
