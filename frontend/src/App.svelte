3534ee1141944c59613a0b9bd5903edccab3a242
Luka <luka.janketic@gmail.com>
2023-08-03 15:58:25 +0200

    Update App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import List from "./components/List.svelte";
+	import CreateNew from "./components/CreateNew.svelte";
 </script>
 
 <main>
 	<h1>Minifyr  -  Shorten your URL</h1>
 	<List />
+	<CreateNew />
 </main>
